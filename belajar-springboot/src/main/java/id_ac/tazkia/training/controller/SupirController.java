507a01b64e8582a6ff6d75d50f3fc92ab1acbe58
Adi Mulya Suprayogi <suprayogi@tazkia.ac.id>
2017-03-23 11:18:19 +0700

    mengubah tampilan

---
+++ belajar-springboot/src/main/java/id_ac/tazkia/training/controller/SupirController.java
@@ -0,0 +1,34 @@
+package id_ac.tazkia.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+
+/**
+ * Created by ECS on 21/03/2017.
+ */
+@Controller
+public class SupirController {
+    @RequestMapping("/supir/list")
+    public ModelMap daftarSupir(@RequestParam(required = false) String nama){
+    ModelMap mm = new ModelMap();
+    mm.addAttribute("sekarang", new Date());
+    mm.addAttribute("cari", nama);
+    return mm;
+    }
+
+    @RequestMapping(value = "/supir/form", method = RequestMethod.GET)
+    public void tampilkanForm(){
+
+    }
+
+
+    @RequestMapping(value = "/supir/form", method = RequestMethod.POST)
+    public String prosesForm(){
+        return "redirect:/supir/list";
+    }
+}
